849089c04c1cc620636d511e597c7993d93b7b9c
amZix01 <flagwork63@gmail.com>
2026-04-07 16:26:48 +0700

    update env path for render platform

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,21 @@ import { ShopModule } from './shop/shop.module';
 import { LoginModule } from './login/login.module';
 import { ArticleModule } from './article/article.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { rejects } from 'assert';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'T12345678',
-      database: 'greenmarket',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'T12345678',
+      database: process.env.DB_NAME || 'greenmarket',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
       charset: 'utf8mb4',
+      ssl: process.env.DB_SSL === 'true' ? {rejectUnauthorized:false} : false ,
     }),
     ShopModule,
     LoginModule,
